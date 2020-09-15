724134aece8a184125e1380ef070839f74a3ef83
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offset/__main__.py
+++ offset/__main__.py
@@ -5,25 +5,41 @@ from offset import __version__, set_node
 
 
 def _build_parser(parser):
-    parser.add_argument('-p', '--password', help='Password for login')
-    parser.add_argument('-i', '--identity-file', help='pem file for login')
+    parser.add_argument("-p", "--password", help="Password for login")
+    parser.add_argument("-i", "--identity-file", help="pem file for login")
 
-    parser.add_argument('--dns-name', '--ip', dest='hostname', help='Location of node', required=True)
-    parser.add_argument('-u', '--user', help='Login username', dest='username', required=True)
-    parser.add_argument('-n', '--name',
-                        help='Name for node. Used for bookkeeping, and other packages may set the hostname to this.')
-    parser.add_argument('--version', action='version',
-                        version='{} {}'.format(modules[__name__].__package__, __version__))
-    parser.add_argument('--etcd', help='Server location\t[127.0.0.1:4001]', default='127.0.0.1:4001')
-    parser.add_argument('--purpose', '--cluster', dest='purpose',
-                        help='Purpose of the node. Groups all together (hierarchically). Defaults to: \'unclustered\'')
+    parser.add_argument(
+        "--dns-name", "--ip", dest="hostname", help="Location of node", required=True
+    )
+    parser.add_argument(
+        "-u", "--user", help="Login username", dest="username", required=True
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="Name for node. Used for bookkeeping, and other packages may set the hostname to this.",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="{} {}".format(modules[__name__].__package__, __version__),
+    )
+    parser.add_argument(
+        "--etcd", help="Server location\t[127.0.0.1:4001]", default="127.0.0.1:4001"
+    )
+    parser.add_argument(
+        "--purpose",
+        "--cluster",
+        dest="purpose",
+        help="Purpose of the node. Groups all together (hierarchically). Defaults to: 'unclustered'",
+    )
     return parser
 
 
-if __name__ == '__main__':
-    pa = ArgumentParser(description='Offset CLI. Set nodes manually.')
+if __name__ == "__main__":
+    pa = ArgumentParser(description="Offset CLI. Set nodes manually.")
     args = _build_parser(pa).parse_args()
 
     if args.password is None and args.identity_file is None:
-        pa.error('at least one of --identity-file and --password required')
+        pa.error("at least one of --identity-file and --password required")
     set_node(**dict(args._get_kwargs()))
